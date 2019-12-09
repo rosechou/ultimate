29ff0bf17a62decd08222fd95a38d298845ac0eb
Samuel <roths@informatik.uni-freiburg.de>
2019-12-09 20:25:33 +0100

    jetty web interface is a application

--- trunk/source/webinterfacejetty/src/main/java/de/uni_freiburg/informatik/ultimate/webinterfacejetty/UltimateWebInterface.java
+++ trunk/source/webinterfacejetty/src/main/java/de/uni_freiburg/informatik/ultimate/webinterfacejetty/UltimateWebInterface.java
@@ -6,8 +6,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class UltimateWebInterface extends HttpServlet
 {
+	private static final String CALLBACK = "callback";
+	/**
+	 * The serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Whether the Servlet should be executed in Debug-Mode or not.
+	 */
+	private static final boolean DEBUG = !false;
+
+//	private final ServletLogger mLogger;
+
+	public UltimateWebInterface() {
+		super();
+	}
+	
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         response.setContentType("text/html");

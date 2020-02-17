41be1dc26eb8f40a082cf57325c42ea607951d1e
Samuel <roths@informatik.uni-freiburg.de>
2020-02-17 12:33:04 +0100

    Refactor build_examples.py

--- trunk/source/WebsiteStatic/build_examples.py
+++ trunk/source/WebsiteStatic/build_examples.py
@@ -5,14 +5,15 @@ import os
 import shutil
 
 import os.path as osp
+
 """ This script copies all available examples for the web ui into config/code_examples. 
-Add new examples to the tool_id section in the tool_examples_map dict following the rules already available.
+Add new examples for a worker in the `tool_examples_map`: Expend the dict at the key `worker_id`. 
+Follow the rules already available.
 """
 
 HERE = osp.abspath(osp.dirname(__file__))
 PROJECT_ROOT = osp.join(HERE, '..', '..')
 EXAMPLES_DIR = osp.join(PROJECT_ROOT, 'examples')
-
 
 tool_examples_map = {
   'boogieAutomizer': [
@@ -129,24 +130,23 @@ tool_examples_map = {
   ],
 }
 
+if __name__ == '__main__':
+    dest = osp.join(HERE, 'config', 'code_examples')
+    if not osp.exists(dest):
+        os.makedirs(dest)
 
-dest = osp.join(HERE, 'config', 'code_examples')
-if not osp.exists(dest):
-  os.makedirs(dest)
+    code_examples = collections.defaultdict(list)
+    for tool, examples in tool_examples_map.items():
+        for example in examples:
+            path = example['path']
+            pattern = example['pattern']
+            for file in glob.glob(rf'{path}/{pattern}'):
+                shutil.copy(file, dest)
+                basename = osp.basename(file)
+                code_examples[tool].append({
+                  'name': (basename[:30] + '...') if len(basename) > 20 else basename,
+                  'source': basename
+                })
 
-
-code_examples = collections.defaultdict(list)
-for tool, examples in tool_examples_map.items():
-    for example in examples:
-        path = example['path']
-        pattern = example['pattern']
-        for file in glob.glob(rf'{path}/{pattern}'):
-            shutil.copy(file, dest)
-            basename = osp.basename(file)
-            code_examples[tool].append({
-              'name': (basename[:30] + '...') if len(basename) > 20 else basename,
-              'source': basename
-            })
-
-with open(osp.join(HERE, 'config', 'code_examples', 'code_examples.json'), mode='w') as out_file:
-  json.dump(code_examples, out_file)
+    with open(osp.join(HERE, 'config', 'code_examples', 'code_examples.json'), mode='w') as out_file:
+        json.dump(code_examples, out_file)

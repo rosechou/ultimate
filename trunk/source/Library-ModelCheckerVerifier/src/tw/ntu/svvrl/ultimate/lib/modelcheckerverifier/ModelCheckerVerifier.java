6291bb6d8801947d3ce879cd7fe096ca04f0b550
rosechou <jcchou100@gmail.com>
2021-05-23 23:44:12 +0800

    Skeleton programming of double DFS

---
+++ trunk/source/Library-ModelCheckerVerifier/src/tw/ntu/svvrl/ultimate/lib/modelcheckerverifier/ModelCheckerVerifier.java
@@ -0,0 +1,158 @@
+package tw.ntu.svvrl.ultimate.lib.modelcheckerverifier;
+
+
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.IncomingInternalTransition;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingInternalTransition;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomatonCache;
+import tw.ntu.svvrl.ultimate.lib.modelcheckerassistant.programstate.ProgramState;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Stack;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.*;
+
+/* The needed API
+ 
+	public static List<ProgramState> get_initials(){
+	// TODO
+	}
+	public static boolean isInitial(ProgramState node){
+		// TODO
+	}
+	public static boolean isAcc(STATE state){
+		// TODO: return true, if the state is accepting state
+	}
+	public static boolean equals(ProgramState node){
+		// TODO
+	}
+	
+	public static Map<String, Object> getSpecValueMap(){
+		// TODO
+	}
+	public static STATE succState(OutgoingInternalTransition tran){
+		// TODO
+	}
+	
+ */
+
+public class ModelCheckerVerifier {
+	//	ProgramState is the node of the Control Flow Graph
+	private final List<ProgramState> levelNodes;
+	// ProgramState preState = null;
+	ProgramState loc = null;
+	ProgramState seed = null;
+	Stack<ProgramState> firstVisited = new Stack<>();
+	Stack<ProgramState> secondVisited = new Stack<>();
+	
+	private final INestedWordAutomaton<CodeBlock, String> mNWA; //<stmt, l0>
+	protected final Set<STATE> initStates;
+	protected final Collection<STATE> finalStates;
+	
+	Stack<Pair> path = new Stack<>();
+	
+	private final ILogger mLogger;
+	
+	private final Map<String, Object> currentTable = new HashMap<>();
+	
+	public ModelCheckerVerifier(final INestedWordAutomaton<CodeBlock, String> nwa)
+	{	
+		mNWA = nwa;
+		initStates = new HashSet<>();
+		finalStates = new ArrayList<>();
+		
+		// set of initial cfg locations
+		levelNodes = new ArrayList<>();
+		levelNodes.addAll(get_initials());
+		
+		// set of initial states of automaton
+		initStates = mNWA.getInitialStates();
+		finalStates = mNWA.getFinalStates();
+		Iterator initIterator = initStates.iterator();
+		STATE init = initIterator.next();
+		
+		dfsBlue(levelNodes, init);
+	}
+	
+	public static void dfsRed(ProgramState node, STATE state)
+	{
+		// TODO
+	}
+		
+	public static void dfsBlue(List<ProgramState> levelNodes, STATE init)
+	{
+		for (final OutgoingInternalTransition<LETTER, STATE> outTrans : internalSuccessors(init)) {
+		anotherTrans:
+			for (int i = 0;i < levelNodes.size();i++) {
+				boolean match = true;
+				
+				// automaton walks one step
+				currentTable = outTrans.getLetter().getSpecValueMap;
+				
+				// cfg walks one step
+				loc = levelNodes.get(i);
+				for(int k = 0;k < firstVisited.size();k++)
+				{
+					if(loc.equals(firstVisited.get(k)))
+					{
+						break anotherTrans;
+					}
+				}
+				firstVisited.push(loc);
+				loc.first = true;
+				
+				// var, value
+				HashMap<String, Object> NodeStatus = loc.getValuationMap().values();
+				List<IcfgEdge> edge = loc.getEnableTrans();
+				
+				// check program satisfies spec, compare currentTable & NodeStatus
+				Iterator currentEntry = currentTable.entrySet().iterator();
+				while(currentEntry.hasNext())
+				{
+					Map.Entry entry = (Map.Entry)currentEntry.next();
+					if(NodeStatus.containsKey(entry.getKey())
+							&&(!NodeStatus.containsValue(entry.getValue())))
+					{
+							match = false;
+							break;
+					}
+				}
+				if(match)
+				{	
+					Pair p = new(loc, init);
+					path.push(p);
+					
+					// automaton move to next state
+					init = init.succState(outTrans);
+					
+					// new a ProgramState
+					// preState = loc;
+					List<ProgramState> nProgramState = new ArrayList<>();
+					for(int j = 0;j < edge.size();j++)
+					{
+						nProgramState.add(doTransition(edge.get(j)));
+					}
+					levelNodes = nProgramState;
+					
+					dfsBlue(levelNodes, init);
+				}			
+			}
+		}
+		if(path.peek().getSecond().isAcc())
+		{
+			seed = path.peek().getFirst();
+			dfsRed(seed, path.peek().getSecond());
+		}
+		path.pop();
+	}
+
+}
